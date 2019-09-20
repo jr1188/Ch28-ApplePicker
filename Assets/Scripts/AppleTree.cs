d3edfc78bba1d17f39a204b9dd85507b4278e44d
Johan Reyes <Johancito23@hotmail.com>
2019-09-19 22:28:47 -0400

    Part 1 - Design, Beginning to Code

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    //Prefab for instantating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+
+    // CONTINUE HERE
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
