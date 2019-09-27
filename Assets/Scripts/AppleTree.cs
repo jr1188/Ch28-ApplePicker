0043be6e2015c017e35beeb2cde22a08d7a25753
Johan Reyes <Johancito23@hotmail.com>
2019-09-27 09:54:57 -0400

    Tree and Apple script completed

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -12,19 +12,56 @@ public class AppleTree : MonoBehaviour
     // Speed at which the AppleTree moves
     public float speed = 1f;
 
+    //Distance where the AppleTree moves
+    public float leftAndRightEdge = 10f;
 
-    // CONTINUE HERE
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Chance that the Apples will be instantained
+    public float secondsBetweenAppleDrops = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
+        //Dropping apples every second
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movment
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+        if ( pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Moves right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Moves left
+        }
+       
     }
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change Direction
+        }
+    }
+        
 }
